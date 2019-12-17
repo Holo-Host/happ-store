1c54a210089f93b54f5678d9353edb90070784de
Perry Kundert <perry@hardconsulting.com>
2019-12-17 06:28:31 -0800

    Upgrade to latest holochain-rust and Rust nightl

--- pkgs.nix
+++ pkgs.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/4a7104e3042f54b1cd454cc7677b3e39e28fac37.tar.gz";
-  sha256 = "1hkdbd49h8q9pqww0h8wvmdc0pymkxd6xqz1bv5xdkisx41s4z43";
+  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/998bc9e6a8e61bde47485e0ef90b51cec70a3786.tar.gz";
+  sha256 = "127021rzh9dq87a6v118c5n6jrjhhxv4hdvvprra21jmgfw8nk73";
 })
